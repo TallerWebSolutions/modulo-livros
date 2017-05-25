b86a1d3beeb59ee84ac3fb5aa68da88203915d6b
Rafael Caceres <rafael@taller.net.br>
2017-05-25 09:03:47 -0300

    Criando queries no banco.

--- src/Controller/LivroController.php
+++ src/Controller/LivroController.php
@@ -11,16 +11,26 @@ class LivroController extends ControllerBase {
    *
    * @return array
    */
-  public function content($id) {
-    $nodeLivro = $this->entityTypeManager()->getStorage('node')->load($id);
-    $nodeLivroView = $this->entityTypeManager()->getViewBuilder('node')->view($nodeLivro, 'default');
+  public function content() {
 
-    return $nodeLivroView;
+    $query = $this->entityTypeManager()->getStorage('node')->getQuery();
+    $query->condition('status', 1);
+    //$query->condition('type', 'livro');
+    //$query->condition('field_autor', 84);
+    $entity_ids = $query->execute();
 
-    return array(
-      '#type' => 'markup',
-      '#markup' => $first,
-    );
+    $livros = $this->entityTypeManager()->getStorage('node')->loadMultiple($entity_ids);
+    $build = $this->entityTypeManager()->getViewBuilder('node')->viewMultiple($livros, 'teaser');
+
+    return $build;
+
+    // https://www.drupal.org/docs/7/api/render-arrays/render-arrays-overview
+    // $build['livros_lista'] = [
+    //   '#theme' => 'item_list',
+    //   // The list itself.
+    //   '#items' => $list,
+    //   '#title' => $this->t('Argument Information'),
+    // ];
   }
 
 }
